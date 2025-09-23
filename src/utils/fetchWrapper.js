676f815c5b791bf5ae8fdbe706df3172067c0c94
AshishG999 <ashishgholap987@gmail.com>
2025-09-23 11:42:16 +0530

    version 1.1.3

--- src/utils/fetchWrapper.js
+++ src/utils/fetchWrapper.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:9500/api/v1';
+console.log('env', process.env.REACT_APP_API_BASE);
 
 export const fetchWrapper = {
   get: (url) => axios.get(`${API_BASE}${url}`),
